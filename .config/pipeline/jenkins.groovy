10c9d38cdef98d3fccd499b5654b2b183c8144a7
shereifsrf <chachushereef@gmail.com>
2023-10-14 19:20:31 +0800

    Test 3

--- .config/pipeline/jenkins.groovy
+++ .config/pipeline/jenkins.groovy
@@ -2,13 +2,8 @@ pipeline {
     agent none
     stages {
         stage('Build') {
-            agent {
-                // python 3.12.0 image
-                docker {
-                    image 'python:3.12.0'
-                }
-            }
             steps {
+                echo 'Building...'
                 sh 'python --version'
             }
         }
